5b3603cd6924f740513c50865628c95456ec9109
Samid <samidms@code.edu.az>
2022-02-16 03:08:25 +0400

    task 5

---
+++ Kontakt Home/ClassLibrary.Tools/Scaner.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Tools
+{
+    public class Scaner
+    {
+        public static string ReadString(string caption)
+        {
+            Console.Write(caption);
+            Console.ForegroundColor = ConsoleColor.Green;
+            string aStr = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return aStr;
+        }
+
+        public static double ReadDouble(string msg)
+        {
+        l1:
+            Console.Write(msg);
+            Console.ForegroundColor = ConsoleColor.Green;
+            string aStr = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            double a;
+
+            try
+            {
+                a = Convert.ToDouble(aStr);
+            }
+            catch
+            {
+                Scaner.PrintError("Yanlish daxil etme, yeniden daxil edin");
+                goto l1;
+            }
+
+            return a;
+        }
+
+        public static int ReadInteger32(string msg)
+        {
+        l1:
+            Console.Write(msg);
+            Console.ForegroundColor = ConsoleColor.Green;
+            string aStr = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            int a;
+
+            try
+            {
+                a = Convert.ToInt32(aStr);
+            }
+            catch
+            {
+                Scaner.PrintError("Yanlish daxiletme, yeniden daxil edin");
+                goto l1;
+            }
+
+            return a;
+        }
+
+        public static void Print(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void PrintError(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Kontakt Home/Kontakt Home.app/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ClassLibrary.Tools;
+
+namespace Kontakt_Home.app
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Phones";
+
+            int len;
+        l1:
+            len = Scaner.ReadInteger32("Nece Telefon Olsun: ");
+            if (len <= 0)
+            {
+                goto l1;
+            }
+            Phones[] stores = new Phones[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Phones phone = new Phones();
+
+                Console.WriteLine($"{i + 1} Telefon Xususiyyetleri: ");
+
+                phone.Model = Scaner.ReadString("Model: ");
+                phone.Producer = Scaner.ReadString("Istehsalatci: ");
+                phone.MainCamera = Scaner.ReadInteger32("Esas Kamera: ");
+                phone.Ram = Scaner.ReadInteger32("Ram: ");
+                phone.Memory = Scaner.ReadInteger32("Yaddas: ");
+                phone.ProductionYear = Scaner.ReadInteger32("Buraxilis Ili: ");
+                phone.OperatingSystem = Scaner.ReadString("Emeliyyat Sistemi: ");
+                phone.BatteryCapacity = Scaner.ReadInteger32("Batareya Tutumu: ");
+                phone.Price = Scaner.ReadDouble("Qiymet: ");
+
+                stores[i] = phone;
+            }
+
+            Scaner.Print("-------------List of Phones--------------");
+
+            foreach (var phone in stores)
+            {
+                Console.WriteLine(phone);
+                Scaner.Print("****************************************************");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
